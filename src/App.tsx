632f87c7388653eaa8f425422bc9ecac8833fdc0
mobinadanshvarweb <mobinadaneshvardeveloper@gmail.com>
2024-10-06 01:11:30 +0330

    edit tasks

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,16 @@ const App = () => {
   return (
     <div className="w-full h-screen bg-blue-100">
       <ul className="">
-        <li>RESponsive</li>
-        <li>pagination</li>
-        <li>scroll like real site</li>
-        <li>when internet is off - delay refresh list</li>
-        <li>edit task</li>
-        <li>toast message</li>
-        <li>sidebar design</li>
-        <li>project</li>
-        <li>section</li>
-        <li>comment</li>
-        <li>how to duplicate sth for example a task</li>
+        <li>1RESponsive</li>
+        <li>2scroll like real site</li>
+        <li>3when internet is off - delay refresh list</li>
+        <li>4edit task</li>
+        <li>5toast message</li>
+        <li>6sidebar design</li>
+        <li>7project</li>
+        <li>8section</li>
+        <li>9comment</li>
+        <li>10how to duplicate sth for example a task</li>
       </ul>
     </div>
   );
